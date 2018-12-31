734b3f27e609fc9ba2ad5dabe78384022aaa0e5f
dooev <dooevdima3@gmail.com>
2018-12-31 07:46:39 +0300

    создание элемента

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import React from 'react'; // обязательная библиотека
 import ReactDOM from 'react-dom'; // обязательная библиотека
+
+const el = (  // создание Реакт элемента (jsx) оборачивается в " (<div>...</div>) " (если тег не один)
+  <div>
+    <h1>Dimasik this is React</h1>
+    <h2>My Todo List</h2>
+
+  </div>
+);
